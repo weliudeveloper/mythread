9117f5807a1aeadb4861226fc9c7d444edc9bfa9
wee <36357651+weliudeveloper@users.noreply.github.com>
2021-07-02 14:32:26 +0800

    pull test

--- src/main/java/com/liuwei/hutool/GitTest.java
+++ src/main/java/com/liuwei/hutool/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("这是测试切换版本4");
         System.out.println("这是测试切换版本3");
         System.out.println("这是测试切换版本3");
+        System.out.println("这是测试切换版本5");
     }
 }
